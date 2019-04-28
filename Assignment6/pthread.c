c046eabd75e12bb04537e8ab180c8831f38fdc61
PiyushSawarkar <44064862+PiyushSawarkar@users.noreply.github.com>
2019-04-28 12:21:00 +0530

    Update pthread.c

--- Assignment6/pthread.c
+++ Assignment6/pthread.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <pthread.h>
-void *functionC();
+
+void * functionC();
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 int counter = 0;
-int main()
-{
- int rc[10];
- pthread_t thread[10];
-int i;
- /* Create independent threads each of which will execute functionC */
- for(i=0;i<=9;i++)
- if( (rc[i]=pthread_create( &thread[i], NULL, &functionC, NULL)) )
- printf("Thread creation failed: %d\n", rc[i]);
+int main() {
+    int rc[10];
+    pthread_t thread[10];
+    int i;
+    /* Create independent threads each of which will execute functionC */
+    for (i = 0; i <= 9; i++)
+        if ((rc[i] = pthread_create( & thread[i], NULL, & functionC, NULL)))
+            printf("Thread creation failed: %d\n", rc[i]);
 
+    /* Wait till threads are complete before main continues. Unless we */
+    /* wait we run the risk of executing an exit which will terminate */
+    /* the process and all threads before the threads have completed. */
+    for (i = 0; i < 10; i++) {
+        pthread_join(thread[i], NULL);
 
-
- /* Wait till threads are complete before main continues. Unless we */
- /* wait we run the risk of executing an exit which will terminate */
- /* the process and all threads before the threads have completed. */
- for(i=0;i<10;i++){
- pthread_join( thread[i], NULL);
-
- }
- exit(0);
+    }
+    exit(0);
 }
-void *functionC()
-{
- pthread_mutex_lock( &mutex1 );
- counter++;
- printf("Counter value: %d\n",counter);
- pthread_mutex_unlock( &mutex1 );
+void * functionC() {
+    pthread_mutex_lock( & mutex1);
+    counter++;
+    printf("Counter value: %d\n", counter);
+    pthread_mutex_unlock( & mutex1);
 }
